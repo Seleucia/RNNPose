d3d2b2306d999505a3411f70f554c612ca73c8fe
Huseyin <coskunh1986@gmail.com>
2016-02-05 00:31:37 +0100

    plot

--- pred.py
+++ pred.py
@@ -14,13 +14,24 @@ params['mfile']= "lstm_model_test_1.p"
 
 
 only_test=1
-params["seq_length"]=100
+params["seq_length"]=300
 (X_test,Y_test,N_list)=du.load_pose(params,only_test)
 BB_list=du.load_test_bboxes(params,len(N_list))
 batch_size=params['batch_size']
 
-n_test_batches = len(X_test)
-n_test_batches /= batch_size
+n_test = len(X_test)
+residual=n_test%batch_size
+if residual>0:
+   na=np.asarray([Y_test[-1]]*residual)
+   Y_test=np.concatenate((Y_test,np.asarray(na)),axis=0)
+
+   na=np.asarray([X_test[-1]]*residual)
+   X_test=np.concatenate((X_test,np.asarray(na)),axis=0)
+
+   n_test = len(Y_test)
+   print("ok")
+
+n_test_batches =n_test/ batch_size
 print("Batch size: %i, test batch size: %i"%(batch_size,n_test_batches))
 print ("Model loading started")
 model= model_provider.get_model_pretrained(params)
@@ -35,6 +46,16 @@ for minibatch_index in range(n_test_batches):
    n_list=N_list[minibatch_index * batch_size*y.shape[1]: (minibatch_index + 1) * batch_size*y.shape[1]]
    bb_list=BB_list[minibatch_index * batch_size*y.shape[1]: (minibatch_index + 1) * batch_size*y.shape[1]]
    pred = model.predictions(x)
+   if residual>0:
+      if(minibatch_index==n_test_batches-1):
+         print len(pred)
+         pred = pred[0:(len(pred)-residual)]
+         y=y[0:(len(y)-residual)]
+         bb_list=bb_list[0:(len(bb_list)-residual)]
+         n_list=n_list[0:(len(n_list)-residual)]
+         print len(pred)
+
+
    du.write_predictions(params,pred,n_list)
    loss=np.mean(np.abs(pred - y))
    (loss3d,bb_loss,l_list) =u.get_loss_bb(y,pred,bb_list)
@@ -48,5 +69,5 @@ batch_bb_loss/=n_test_batches
 s ='error %f, %f, %f '%(batch_loss,batch_loss3d,batch_bb_loss)
 print (s)
 pu.plot_histograms(loss_list)
-pu.plot_error_frame(loss)
+pu.plot_error_frame(loss_list)
 pu.plot_cumsum(loss_list)
