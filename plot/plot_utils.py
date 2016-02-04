1e4f180f84e41bd63ef481be2b6c69f2e554e709
Huseyin <coskunh1986@gmail.com>
2016-02-04 21:30:57 +0100

    plot

--- plot/plot_utils.py
+++ plot/plot_utils.py
@@ -5,6 +5,6 @@ def plot_histograms(loss):
    fig = plt.figure()
    ax = fig.add_subplot(111)
    x = loss
-   bins = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,18,20)
+   bins = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,18,20,30,50)
    ax.hist(x,bins=bins,color='green',alpha=0.8)
    plt.show()
