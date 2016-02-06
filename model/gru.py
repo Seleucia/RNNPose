21072ff1a65de628a1f3af191ae1e2fc5279b0ce
Huseyin <coskunh1986@gmail.com>
2016-02-06 21:41:29 +0100

    plot

--- model/gru.py
+++ model/gru.py
@@ -12,47 +12,38 @@ class gru:
        self.n_in = n_in
        self.n_lstm = n_lstm
        self.n_out = n_out
-       self.W_xi = init_weight((self.n_in, self.n_lstm),'W_xi', 'glorot')
-       self.W_hi = init_weight((self.n_lstm, self.n_lstm),'W_hi', 'ortho')
-       self.W_ci = init_pweight((self.n_lstm, self.n_lstm),'W_ci', 'glorot')
-       self.b_i  = init_bias(self.n_lstm, sample='zero')
-       self.W_xf = init_weight((self.n_in, self.n_lstm),'W_xf', 'glorot')
-       self.W_hf = init_weight((self.n_lstm, self.n_lstm),'W_hf', 'ortho')
-       self.W_cf = init_pweight((self.n_lstm, self.n_lstm),'W_cf', 'glorot')
-       self.b_f = init_bias(self.n_lstm, sample='one')
-       self.W_xc = init_weight((self.n_in, self.n_lstm),'W_xc', 'glorot')
-       self.W_hc = init_weight((self.n_lstm, self.n_lstm),'W_hc', 'ortho')
-       self.b_c = init_bias(self.n_lstm, sample='zero')
-       self.W_xo = init_weight((self.n_in, self.n_lstm),'W_xo', 'glorot')
-       self.W_ho = init_weight((self.n_lstm, self.n_lstm),'W_ho', 'ortho')
-       self.W_co = init_pweight((self.n_lstm, self.n_lstm),'W_co', 'glorot')
-       self.b_o = init_bias(self.n_lstm, sample='zero')
+       self.W_xr = init_weight((self.n_in, self.n_lstm), 'W_xi', 'glorot')
+       self.W_hr = init_weight((self.n_lstm, self.n_lstm), 'W_hi', 'ortho')
+       self.b_r  = init_bias(self.n_lstm, sample='zero')
+       self.W_xz = init_weight((self.n_in, self.n_lstm), 'W_xf', 'glorot')
+       self.W_hz = init_weight((self.n_lstm, self.n_lstm), 'W_hf', 'ortho')
+       self.b_z = init_bias(self.n_lstm, sample='zero')
+       self.W_xh = init_weight((self.n_in, self.n_lstm), 'W_xc', 'glorot')
+       self.W_hh = init_weight((self.n_lstm, self.n_lstm), 'W_hc', 'ortho')
+       self.b_h = init_bias(self.n_lstm, sample='zero')
        self.W_hy = init_weight((self.n_lstm, self.n_out),'W_hy', 'glorot')
        self.b_y = init_bias(self.n_out, sample='zero')
 
-       self.params = [self.W_xi, self.W_hi, self.W_ci, self.b_i,
-                      self.W_xf, self.W_hf, self.W_cf, self.b_f,
-                      self.W_xc, self.W_hc, self.b_c,  self.W_xo,
-                      self.W_ho, self.W_co, self.b_o,
+       self.params = [self.W_xr, self.W_hr, self.b_r,
+                      self.W_xz, self.W_hz, self.b_z,
+                      self.W_xh, self.W_hh, self.b_h,
                       self.W_hy, self.b_y]
 
-       def step_lstm(x_t, h_tm1, c_tm1):
-           i_t = T.nnet.sigmoid(T.dot(x_t, self.W_xi) + T.dot(h_tm1, self.W_hi) + c_tm1*self.W_ci + self.b_i)
-           f_t = T.nnet.sigmoid(T.dot(x_t, self.W_xf) + T.dot(h_tm1, self.W_hf) + c_tm1* self.W_cf + self.b_f)
-           c_t = f_t * c_tm1 + i_t * T.tanh(T.dot(x_t, self.W_xc) + T.dot(h_tm1, self.W_hc) + self.b_c)
-           o_t = T.nnet.sigmoid(T.dot(x_t, self.W_xo)+ T.dot(h_tm1, self.W_ho) + c_t*self.W_co  + self.b_o)
-           h_t = o_t * T.tanh(c_t)
-           y_t = T.nnet.sigmoid(T.dot(h_t, self.W_hy) + self.b_y)
-           return [h_t, c_t, y_t]
+       def step_lstm(x_t, h_tm1):
+           r_t = T.nnet.sigmoid(T.dot(x_t, self.W_xr) + T.dot(h_tm1, self.W_hr) + self.b_r)
+           z_t = T.nnet.sigmoid(T.dot(x_t, self.W_xz) + T.dot(h_tm1, self.W_hz)  + self.b_z)
+           h_t = T.nnet.sigmoid(T.dot(x_t, self.W_xh) + T.dot((r_t*h_tm1),self.W_hh)  + self.b_h)
+           hh_t = z_t * h_tm1 + (1-z_t)*h_t
+           y_t = T.nnet.sigmoid(T.dot(hh_t, self.W_hy) + self.b_y)
+           return [h_t, y_t]
 
        X = T.tensor3() # batch of sequence of vector
        Y = T.tensor3() # batch of sequence of vector (should be 0 when X is not null)
        h0 = shared(np.zeros(shape=(batch_size,self.n_lstm), dtype=dtype)) # initial hidden state
-       c0 = shared(np.zeros(shape=(batch_size,self.n_lstm), dtype=dtype)) # initial hidden state
 
-       [h_vals, c_vals, y_vals], _ = theano.scan(fn=step_lstm,
+       [h_vals, y_vals], _ = theano.scan(fn=step_lstm,
                                          sequences=X.dimshuffle(1,0,2),
-                                         outputs_info=[h0, c0, None])
+                                         outputs_info=[h0, None])
 
        self.output = y_vals.dimshuffle(1,0,2)
        cxe = T.mean(T.nnet.binary_crossentropy(self.output, Y))
@@ -75,4 +66,4 @@ class gru:
 
        self.train = theano.function(inputs=[X, Y],outputs=cost,updates=_optimizer.getUpdates(),allow_input_downcast=True)
        self.predictions = theano.function(inputs = [X], outputs = y_vals.dimshuffle(1,0,2),allow_input_downcast=True)
-       self.n_param=n_lstm*n_lstm*4+n_in*n_lstm*4+n_lstm*n_out+n_lstm*3
+       self.n_param=n_lstm*n_lstm*3+n_in*n_lstm*3+n_lstm*n_out+n_lstm*3
--- model/model_provider.py
+++ model/model_provider.py
@@ -2,6 +2,7 @@ from model.lstm import  lstm
 from model.lstmnp import  lstmnp
 from model.lstm2 import lstm2
 from model.erd import erd
+from model.gru import gru
 from model.erd_pre import erd_pre
 from helper.optimizer import ClipRMSprop, RMSprop
 import helper.utils as u
@@ -18,6 +19,8 @@ def get_model(params):
         model = erd(1024, params['n_hidden'], 54,batch_size=params['batch_size'],lr=params['initial_learning_rate'],output_activation=theano.tensor.nnet.sigmoid, cost_function='mse')
     elif(params["model"]=="erd_pre"):
         model = erd_pre(1024, params['n_hidden'], 54,batch_size=params['batch_size'],lr=params['initial_learning_rate'],output_activation=theano.tensor.nnet.sigmoid, cost_function='mse')
+    elif(params["model"]=="gru"):
+        model = gru(1024, params['n_hidden'], 54,batch_size=params['batch_size'],lr=params['initial_learning_rate'],output_activation=theano.tensor.nnet.sigmoid, cost_function='mse',optimizer=RMSprop)
     else:
         model=None
     return model
